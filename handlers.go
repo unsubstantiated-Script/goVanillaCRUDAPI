e324f1af0843ad74c472ccda383558beebab497a
unsubstantiated-Script <jusfulton@gmail.com>
2025-07-04 14:21:04 -0700

    Product handler plus route adjustments

--- handlers.go
+++ handlers.go
@@ -30,3 +30,50 @@ func ProductsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
+
+func ProductHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	switch r.Method {
+	case http.MethodGet:
+		//Handle get a single product by ID. Loop through items, find the item in the array (if it exits) return item as json
+		for _, p := range Products {
+			if p.ID == id {
+				err := json.NewEncoder(w).Encode(p)
+				if err != nil {
+					return
+				}
+				return
+			}
+		}
+		http.Error(w, "Product not found", http.StatusNotFound)
+	case http.MethodPut:
+		var updatedProduct Product
+		err := json.NewDecoder(r.Body).Decode(&updatedProduct)
+		if err != nil {
+			return
+		}
+		for i, p := range Products {
+			if p.ID == id {
+				Products[i] = updatedProduct
+				err := json.NewEncoder(w).Encode(Products[i])
+				if err != nil {
+					return
+				}
+				return
+			}
+		}
+		http.Error(w, "Product not found", http.StatusNotFound)
+	case http.MethodDelete:
+		for i, p := range Products {
+			if p.ID == id {
+				Products = append(Products[:i], Products[i+1:]...)
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+		}
+		http.Error(w, "Product not found", http.StatusNotFound)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
